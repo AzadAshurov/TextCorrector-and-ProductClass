383eb00a01fba2746c84cacaa21c68de829b523a
Wertdrof <wertdrof@gmail.com>
2024-10-15 00:54:48 +0400

    Finally some sleep

---
+++ TextCorrector-Product/Product.cs
@@ -0,0 +1,43 @@
+﻿namespace TextCorrector_Product
+{
+    internal class Product
+    {
+        public string BrandName;
+        public string Model;
+        public int Price;
+        public int Cost;
+        public int Count;
+        public string Id;
+        public int Income;
+        public Product(string brandName, string model, int price, int cost, int count)
+        {
+            BrandName = brandName;
+            Model = model;
+            Price = price;
+            Cost = cost;
+            Count = count;
+            if (Count < 0)
+            {
+                Count = 0;
+            }
+            Id = BrandName.Substring(0, 2) + Model.Substring(0, 2);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Name of the brand: {BrandName},Model: {Model},Price: {Price}, Cost: {Cost}, Count: {Count}, Id: {Id}");
+        }
+        public void Sale()
+        {
+            if (Count > 0)
+            {
+                Count--;
+                Income = Income + Price - Cost;
+                Console.WriteLine($"Item is sold, total income is: {Income} and items left: {Count}");
+            }
+            else
+            {
+                Console.WriteLine("Sorry! This product already sold");
+            }
+        }
+    }
+}
---
+++ TextCorrector-Product/Program.cs
@@ -0,0 +1,111 @@
+﻿namespace TextCorrector_Product
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Product product = null;
+            Console.WriteLine("Enter 1 for first task and 2 for second.Enter something else programm to stop programm");
+            string option;
+            do
+            {
+                option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        Console.WriteLine("Enter your text");
+                        string CorrectedText = TextCorrector(Console.ReadLine());
+                        Console.WriteLine(CorrectedText);
+                        break;
+                    case "2":
+                        string option2;
+                        do
+                        {
+                            Console.WriteLine("Enter 1 for new product, 2 for getting info about product, 3 for sale");
+                            option2 = Console.ReadLine();
+                            switch (option2)
+                            {
+
+                                case "1":
+                                    Console.WriteLine("Enter Brand Name , Model, Price, Cost, Count");
+                                    string brandName = Console.ReadLine();
+                                    string model = Console.ReadLine();
+                                    int price = Convert.ToInt32(Console.ReadLine());
+                                    int cost = Convert.ToInt32(Console.ReadLine());
+                                    int count = Convert.ToInt32(Console.ReadLine());
+                                    product = new Product(brandName, model, price, cost, count);
+                                    break;
+                                case "2":
+                                    if (product == null)
+                                    {
+                                        Console.WriteLine("No product was added, please enter 1 and add some item");
+                                    }
+                                    else
+                                    {
+                                        product.GetInfo();
+                                    }
+                                    break;
+                                case "3":
+                                    if (product == null)
+                                    {
+                                        Console.WriteLine("No product was added, please enter 1 and add some item");
+                                    }
+                                    else
+                                    {
+                                        product.Sale();
+                                    }
+                                    break;
+                            }
+                        } while (option2 == "1" || option2 == "2" || option2 == "3");
+                        Console.WriteLine("Task two ended succsessfully");
+
+
+                        break;
+                }
+            }
+            while (option == "1" || option == "2");
+            Console.WriteLine("Programm ended successfully");
+        }
+
+        static string TextCorrector(string text)
+        {
+            text = text.Trim();
+            if (text.Length == 0)
+            {
+                text = "Nothing were texted";
+            }
+            string newText = "";
+            bool isSpace = false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    if (!isSpace)
+                    {
+                        newText += text[i];
+                    }
+                    isSpace = true;
+                }
+                else
+                {
+                    isSpace = false;
+                    newText += text[i];
+                }
+
+            }
+
+            string[] words = newText.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToLower();
+                words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1);
+            }
+            text = String.Join(" ", words);
+            return text;
+        }
+
+
+
+    }
+}
